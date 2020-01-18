ac759d1c2fa4e3a45c53a66a73d85cc4c9c1e6ca
gentavdijaj <avdijajgent@gmail.com>
2020-01-18 01:56:47 +0100

    Initial commit

---
+++ src/main/java/com/gent/springwebap/SpringwebapApplication.java
@@ -0,0 +1,13 @@
+package com.gent.springwebap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gent/springwebap/SpringwebapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gent.springwebap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
